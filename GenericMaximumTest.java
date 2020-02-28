a0cccb9fd158e788a40670f67c322783fe349fc6
aashishogale <aashishogale9@gmail.com>
2020-02-28 15:56:41 +0530

    changed assertion

--- GenericMaximumTest.java
+++ GenericMaximumTest.java
@@ -5,11 +5,20 @@ import org.junit.Test;
 public class GenericMaximumTest {
 
     @Test
-    public  void test_max_integer_from_generic_maximum() {
+    public  void test_first_integer_from_generic_maximum() {
 
         GenericMaximum  gen_max = new GenericMaximum();
-        Assert.assertSame(3, gen_max.maxInteger(1,2,3));
+        Assert.assertSame(3, gen_max.maxInteger(3,2,1));
     }
+
+    @Test
+    public  void second_first_integer_from_generic_maximum() {
+
+        GenericMaximum  gen_max = new GenericMaximum();
+        Assert.assertSame(2, gen_max.maxInteger(1,2,1));
+    }
+
+
 
 
 
