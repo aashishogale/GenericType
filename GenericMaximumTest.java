8f8922b372b8279cd3f49bd23e71c3eb8a1bfa18
aashishogale <aashishogale9@gmail.com>
2020-02-28 16:02:24 +0530

    added three test cases

--- GenericMaximumTest.java
+++ GenericMaximumTest.java
@@ -1,25 +1,23 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class GenericMaximumTest {
+
+    GenericMaximum  gen_max = new GenericMaximum();
 
     @Test
     public  void test_first_integer_from_generic_maximum() {
-
-        GenericMaximum  gen_max = new GenericMaximum();
         Assert.assertSame(3, gen_max.maxInteger(3,2,1));
     }
 
     @Test
     public  void second_first_integer_from_generic_maximum() {
-
-        GenericMaximum  gen_max = new GenericMaximum();
         Assert.assertSame(2, gen_max.maxInteger(1,2,1));
     }
 
-
-
-
+    @Test
+    public  void third_first_integer_from_generic_maximum() {
+        Assert.assertSame(3, gen_max.maxInteger(1,2,3));
+    }
 
 }
